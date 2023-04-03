0e73911f7b191a0c3f2515fc6836c9b762a03c8a
artemblockczech <93347064+artemblockczech@users.noreply.github.com>
2023-04-03 11:12:05 +0200

    Create mintNFT2.js

---
+++ mintNFT2.js
@@ -0,0 +1,37 @@
+// in ENV file
+// API_URL = "ALCHEMY_API_ENDPOINT”
+// PRIVATE_KEY = "WALLET_PRIVATE_ADDRESS"
+// PUBLIC_KEY = "WALLET_PUBLIC_ADDRESS"
+
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+const contractAddress = "CONTRACT_ADDRESS";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //the transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 2999999987,
+    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+  };
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  
+  console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+}
+
+mintNFT("https://ipfs.filebase.io/ipfs/FILEBASE_IPFS_CID");
